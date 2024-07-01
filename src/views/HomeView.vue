8fd9ce1fd4f007ba352066df690274ff5889a9d0
Germel Marata <germeljohnmarata@gmail.com>
2024-07-01 17:29:39 +0800

    first init

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,58 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div ref="nav" id="nav" class="section">
+          <Nav />
+    </div>
+    <div ref="scene" id="scene" class="section">
+      <Scene />
+    </div>
+    
+    <div ref="project" id="project" class="section">
+      <Project />
+    </div>
+    
+    <div ref="about" id="about">
+      <About />
+    </div>
+
+    <div ref="contact" id="contact">
+      <Contact />
+    </div>
+
+    <div ref="footer" id="footer">
+      <Footer />
+    </div>
+    
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { defineComponent } from 'vue';
 
-export default {
+// Components
+import Nav from '../components/Navigation.vue';
+import Scene from '../components/threeScene.vue';
+import Project from '../components/Projects.vue';
+import About from './AboutWriter.vue'
+import Contact from './ContactView.vue'
+import Footer from '../components/Footer.vue'
+
+
+export default defineComponent({
   name: 'HomeView',
+
   components: {
-    HelloWorld
-  }
-}
+    Nav,
+    Scene,
+    Project,
+    About,
+    Contact,
+    Footer
+  
+    
+  },
+});
 </script>
+
+
+
